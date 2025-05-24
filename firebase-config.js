380c95b7f4e874a06ab41df77d7743070b760876
AshishBachan <120729921+Ashishbachan14@users.noreply.github.com>
2025-05-24 15:09:58 +0530

    Added Main Files

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBVsayKdNMw_n6sOnCivUeKcFVuHkO4Lew",
+  authDomain: "fire-smoke-sensor.firebaseapp.com",
+  databaseURL:
+    "https://fire-smoke-sensor-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "fire-smoke-sensor",
+  storageBucket: "fire-smoke-sensor.firebasestorage.app",
+  messagingSenderId: "741590514485",
+  appId: "1:741590514485:web:5a592fcfcef3dcc8c42a7c",
+};
